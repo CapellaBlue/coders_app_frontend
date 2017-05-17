5414ee6d97dd0178a355a7518f4cd63ee7418827
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-17 16:28:48 -0400

    AJAX request GET route works

--- public/app.js
+++ public/app.js
@@ -4,10 +4,40 @@ var app = angular.module('politicalBoard', []);
 
 app.controller('mainController', ['$http', function($http){
     this.message = "angular works!";
+    this.posts = [];
+    this.viewPost = {}
+    this.postComments = [];
+    this.viewOnePost = false;
+
+    // GET All Posts
     $http({
         method: 'GET',
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
         url: 'http://localhost:3000/posts'
     }).then(function(response){
-        console.log(response);
-    });
+        console.log(response.data);
+        this.posts = response.data;
+    }.bind(this));
+
+    // See one Post and its Comments
+    this.showPostComments = function(post_id, ind){
+      this.viewPost = this.posts[ind];
+      $http({
+          method: 'GET',
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+          },
+          url: 'http://localhost:3000/posts/'+post_id+'comments'
+      }).then(function(response){
+        console.log(response.data.comments);
+        this.postComments = response.data.comments;
+        this.viewOnePost = true;
+      }.bind(this));
+
+    };
+
+
+
 }]);
