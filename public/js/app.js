f60556074e01d3d8ab004d69f1d77e92935c09a3
Amanda Capella <mrs.acapella@gmail.com>
2017-05-18 14:15:08 -0700

    I like it. Bootstrap columns solved

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,13 @@ var app = angular.module('politicalBoard', []);
 
 app.controller('mainController', ['$http', function($http){
     // this.message = "angular works!";
+    this.attr_id = "";
     this.posts = [];
     this.viewPost = {}
     this.postComments = [];
     this.viewOnePost = false;
     this.postFormdata = {};
+    this.affiliation = ["Hard Right", "Soft Right", "Centrist", "Soft Left", "Hard Left", "Independent"];
 
     // GET All Posts
     $http({
@@ -20,6 +22,30 @@ app.controller('mainController', ['$http', function($http){
     }).then(function(response){
         console.log(response.data);
         this.posts = response.data;
+        for (var i = 0; i < response.data.length; i++) {
+            var aff = response.data[i].political_affiliation;
+
+            if(aff == "Hard Right"){
+                response.data[i].political_affiliation = "hard-right";
+            } else if(aff == "Soft Right"){
+                response.data[i].political_affiliation = "soft-right";
+            } else if(aff == "Hard Left"){
+                response.data[i].political_affiliation = "hard-left";
+            } else if(aff == "Soft Left"){
+                response.data[i].political_affiliation = "soft-left";
+            } else if(aff == "Centrist"){
+                response.data[i].political_affiliation = "centrist";
+            } else if(aff == "Independent"){
+                response.data[i].political_affiliation = "independent";
+            };
+            console.log(aff);
+            console.log(response.data[i].political_affiliation);
+
+
+
+        };
+
+
     }.bind(this));
 
     // See one Post and its Comments
@@ -38,6 +64,7 @@ app.controller('mainController', ['$http', function($http){
       }.bind(this));
 
     };
+//create POST route
     this.createPost = function(){
         console.log("inside createPost: ", this.postFormdata);
         $http({
@@ -50,7 +77,22 @@ app.controller('mainController', ['$http', function($http){
             this.posts.unshift(result.data);
         }.bind(this));
     };
-
+    //carousel
+    this.myInterval = 3000;
+    this.slides = [
+    {
+      image: 'http://lorempixel.com/400/200/'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/food'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/sports'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/people'
+    }
+  ];
 
 
 }]);
